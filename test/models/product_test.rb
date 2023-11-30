19adf429652585a4a5915d4a9b580c5f0175706d
Vyacheslav Odinokov <vyacheslav.odinokov@simourg.com>
2023-11-30 15:54:52 +0000

    feat: added tests for product module

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Test Book",
+                          description: "My Description",
+                          image_url: "image.png")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.0"], product.errors[:price]
+
+    product.price = -0.00001
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.0"], product.errors[:price]
+    
+    product.price = 0.00
+    assert product.valid?
+  end
+
+  test "image url must be valid" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://aa.bb.cc/x/y/z/fred.GIF }
+    bad = %w{ fred.doc fred fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product_with_image(image_url).valid?, "#{image_url} must be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product_with_image(image_url).invalid?, "#{image_url} must be invalid"
+    end
+  end
+
+  def new_product_with_image(image_url)
+    Product.new(title: "My Test Book",
+                description: "My Description",
+                price: 0,
+                image_url: image_url)
+  end
 end

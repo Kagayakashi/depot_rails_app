3b4198149717fbec42cd3df395d0c07cb56af88f
Vyacheslav Odinokov <vyacheslav.odinokov@simourg.com>
2023-12-19 12:51:57 +0000

    test: added functional tests for store controller index

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,5 +6,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select "nav a", minimum: 4
+    assert_select "main ul li", 5
+    assert_select "h2", "Captain Blood"
+    assert_select "h2", "The Count Monte Cristo"
+    assert_select "h2", "Disgardium Class-A Threat"
   end
 end
